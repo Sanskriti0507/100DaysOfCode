e101f44781443aa51c4b4f2a484cdf7bc0174690
Sanskriti0507 <sanskritias57@gmail.com>
2025-08-28 00:22:46 +0530

    Add files via upload

---
+++ ques13.c
@@ -0,0 +1,48 @@
+// Q13: Write a program to input a year and check whether it is a leap year or not using conditional statements.
+
+/*
+Sample Test Cases:
+Input 1:
+2020
+Output 1:
+Leap year
+
+Input 2:
+1900
+Output 2:
+Not a leap year
+
+Input 3:
+2000
+Output 3:
+Leap year
+
+*/
+
+
+
+
+
+
+#include<stdio.h>
+int main(){
+int a;
+
+printf("Year: ");
+scanf("%d", &a);
+
+if(a%400==0){
+printf("It is a leap year.");
+}
+else if(a%4==0){
+printf("It is a leap year.");
+}
+else if(a%100==0){
+printf("It is not a leap year.");
+}
+else{
+printf("It is not a leap year.");
+}
+
+return 0;
+}
